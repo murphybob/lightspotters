bd44048d3abfe81a914d526a3895299b5a124b2b
Rob Murphy <rob.murphy@wegift.io>
2020-11-30 07:42:19 +0000

    Replace django with flask

--- lightspotters/wsgi.py
+++ lightspotters/wsgi.py
@@ -1,16 +1,1 @@
-"""
-WSGI config for lightspotters project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
-
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lightspotters.settings")
-
-from django.core.wsgi import get_wsgi_application
-
-application = get_wsgi_application()
+from lightspotters.app import app as application
